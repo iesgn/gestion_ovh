599759a5caa1ec07edab56137749f76d5c493772
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2020-10-22 21:19:30 +0200

    los dos programas juntos

--- cambiar_contacto_tecnico.py
+++ cambiar_contacto_tecnico.py
@@ -46,3 +46,27 @@ try:
         )
 except:
     print("Error. Nombre de correo de dominio o usuario incorrecto.")
+
+
+
+try:
+    result = client.get('/me/task/contactChange', 
+        toAccount=id, # Filter the value of toAccount property (like) (type: string)
+    )
+except:
+    print("Usuario incorrecto.")
+    result=[]
+
+
+for servicio in result:
+
+    result2 = client.get('/me/task/contactChange/%s' % servicio)
+    #print(json.dumps(result2, indent=4))
+    print(result2.get("serviceDomain"),"(",result2.get("id"),")")
+    token = input("Token:")
+    try:
+        result = client.post('/me/task/contactChange/%s/accept' % result2.get("id"), 
+            token=token, # The token you received by email for this request (type: string)
+        )
+    except:
+        print("Error al validar el token.")
